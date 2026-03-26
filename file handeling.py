56c7df8afd44d168b365c6be136b3d47cf34a4b2
Rohan Pawar <rohanpawar2006.1@gmail.com>
2026-03-27 03:39:44 +0530

    Add files via upload

---
+++ file handeling.py
@@ -0,0 +1,13 @@
+file=open("myfile.txt","w")
+file.write("hello student")
+file.close()
+
+file=open("myfile.txt","a")
+file.write("\n Welcome to python")
+file.close
+
+file=open("myfile.txt","r")
+data=file.read()
+print=data
+file.close
+
